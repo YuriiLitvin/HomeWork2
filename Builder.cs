db4fcac5d5597c9c070825fb3d22ccdcd8cdcd70
Yurii Litvin <yurijlitviniii@gmail.com>
2020-06-18 13:23:26 +0300

    rollback to comments

--- Builder.cs
+++ Builder.cs
@@ -3,59 +3,53 @@ using System.Collections.Generic;
 
 namespace BuildingHouse
 {
-    class Builder : Worker
+    class Builder : IWorker
     {
-       
-        // this method does ANY pair, not the one located at partToDoIndex
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Energy { get; set; }
+
+
+
+        // this method does ANY part, not the one located at partToDoIndex
         // is partToDoIndex is index to start with? or is it unnecessary at all?
         // in case its index -- use specification.Skip(index)
         // otherwise -- remove
-        public bool GetToWork(List<IPart> specification)
-        {
-            if (Energy >= 80)
-            {
-                this.DoWork(specification);
-            }
-            else
-            {
-                this.GetDayOff();
-            }
-            return false;
-        }
-        
-        
-        
-        
-        
-        public bool DoWork(List<IPart> specification)
+        public bool DoWork(Dictionary<int, IPart> specification, int partToDoIndex)
         {
             // in case you don`t really need Key here (see comment above)
             // consider
-            foreach (var part in specification)
+            foreach (KeyValuePair<int, IPart> part in specification)
             {
-                // DONE: use && instead of & (its lazy and, there is lazy or (||) as well
+                // use && instead of & (its lazy and, there is lazy or (||) as well
 
-                // DONE: you refer pair.Value too much, please do:
-                var part = pair.Value;
-                if (pair.Key == partToDoIndex && !part.IsDone)
+                // you refer part.Value too much, please do:
+                // var part = pair.Value;
+                if (part.Key == partToDoIndex & !part.Value.IsDone)
                 {
-                    Console.WriteLine($"**************I completed {part.Name}\n");
-                    part.IsDone = true;
-                    return part.IsDone;
+                    Console.WriteLine($"**************I completed {part.Value.Name}\n");
+                    part.Value.IsDone = true;
+                    return part.Value.IsDone;
                 }
-                else if (part.IsDone)
+
+                else if (part.Value.IsDone)
                 {
                 }
                 else
                 {
                     Console.WriteLine(
-                        $"I can't do {part.Name}" +
+                        $"I can't do {part.Value.Name}" +
                         $" because {specification[partToDoIndex].Name} is not completed");
                 }
             }
             
             return false;
         }
-        
+        public bool GetDayOff()
+        {
+            Console.WriteLine("I can't work now. I need day off");
+            return false;
+        }
+
     }
 }
