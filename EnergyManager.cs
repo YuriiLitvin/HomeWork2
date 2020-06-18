f5827acacbf6d40529c274148c6c333e3d262b3b
Yurii Litvin <yurijlitviniii@gmail.com>
2020-06-18 16:26:19 +0300

    class EnergyManager has changes

--- EnergyManager.cs
+++ EnergyManager.cs
@@ -3,23 +3,48 @@
 
 namespace BuildingHouse
 {
-    public static class EnergyManager
+    public class EnergyManager
     {
-        public static int SetEnergyLevel(bool isDone, int Energy)
+        public int Energy;
+
+        public const int minEnergyLevel = 80;
+        public const int maxEnergyLevel = 100;
+        public const int increaseMin = 5;
+        public const int increaseMax = 15;
+        public const int decreaseMin = 10;
+        public const int decreaseMax = 20;
+
+        Random random = new Random();
+
+        public bool CanDoWork()
+        {
+            if (Energy >= minEnergyLevel)
+                return true;
+            return false;
+        }
+
+        public int GetEnergyLevel() => Energy = random.Next(minEnergyLevel, maxEnergyLevel);
+
+        public int Increase() => Energy += random.Next(increaseMin, increaseMax);
+        public int Decrease() => Energy -= random.Next(decreaseMin, decreaseMax);
+
+
+
+        public int SetEnergyLevel(bool isDone, int Energy)
         {
             // all randoms should be static;
             // in case you need to use random.Next(x, y) many times
             // make a wrapper in randomizer class
-            Random random = new Random();
+
             if (isDone)
             {
                 // 10 is increaseMin, 20 is increaseMax and they are constant!
-                Energy -= random.Next(10, 20);
+                Energy -= random.Next(decreaseMin, decreaseMax);
             }
             else
             {
                 // same here
-                Energy += random.Next(5, 15);
+                Energy += random.Next(increaseMin, increaseMax);
             }
             return Energy;
         }
